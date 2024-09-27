f3a3daab65b9ca7b4fe54fae67b142064337a907
Sandusha20049887 <sandusha1997@gmail.com>
2024-09-27 14:17:49 +0100

    param

--- euler.js
+++ euler.js
@@ -1,5 +1,5 @@
-alert("Euler loaded!");
-funtionSumOfMultiples = () => {
+//alert("Euler loaded!");
+funtionSumOfMultiplies = () => {
 
     let sum = 0;
     for(let i = 0; i < 1000; i++){
@@ -12,3 +12,15 @@ funtionSumOfMultiples = () => {
 }
 
 funtionSumOfMultiples();
+
+funtionSumOfMultipliesParam = (a,b) => {
+
+    let sum = 0;
+    for(let i = 0; i < 1000; i++){
+        if( (i % a == 0) || (i % b == 0)){
+            sum += i;
+        }
+    }
+
+    alert("Sum of multiples of "+a+ " and " +b+ " below 1000 : "+sum);
+}
