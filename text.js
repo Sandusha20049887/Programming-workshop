4437c3b0c9f22fa4b9b2e3c9ff6b0838a51587fa
Sandusha20049887 <sandusha1997@gmail.com>
2024-09-27 13:51:12 +0100

    euler

--- text.js
+++ text.js
@@ -18,14 +18,3 @@ myFunction = () =>{
     }
 }
 
-funtionSumOfMultiples = () => {
-
-    let sum = 0;
-    for(let i = 0; i < 10; i++){
-        if( (i % 3 == 0) || (i % 5==0)){
-            sum += i;
-        }
-    }
-
-    alert("Sum of multiples of 3 and 5 below 10 : "+sum);
-}
