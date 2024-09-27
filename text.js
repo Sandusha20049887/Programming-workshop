7a267e0242a0770c574fa14f6a8d6accf2afb02c
Sandusha20049887 <sandusha1997@gmail.com>
2024-09-27 13:48:32 +0100

    r

--- text.js
+++ text.js
@@ -23,7 +23,7 @@ funtionSumOfMultiples = () => {
     let sum = 0;
     for(let i = 0; i < 10; i++){
         if( (i % 3 == 0) || (i % 5==0)){
-            sum =+ i;
+            sum += i;
         }
     }
 
